8bb7863926247fce6545e1deaf1e012b0c6ef485
Dima <vidistudioproduction@gmail.com>
2023-04-06 23:18:10 +0300

    module 10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const elements = {
+  searchBox : document.querySelector('#search-box'),
+  countryList : document.querySelector('.country-list'),
+  countryInfo : document.querySelector('.country-info'),
+}
+
+elements.searchBox.addEventListener('input', debounce(onSerchCountries, DEBOUNCE_DELAY));
+
+function onSerchCountries(event) {
+  let serchName = event.target.value.trim();
+
+  if (serchName === '') return;
+
+  fetchCountries(serchName)
+  .then(countries => {
+    elements.countryList.innerHTML = '';
+    elements.countryInfo.innerHTML = '';
+
+    if (countries.length === 1) {
+      elements.countryList.innerHTML = renderCountriesList(countries);
+      elements.countryInfo.innerHTML = renderCountryInfo(countries);
+      console.log(countries);
+
+    } else if (countries.length >= 2 && countries.length <= 10) {
+      elements.countryList.innerHTML = renderCountriesList(countries);
+
+    } else {
+      Notify.info('Too many matches found. Please enter a more specific name.');
+    }
+  })
+  .catch(() => {
+    Notify.failure('Oops, there is no country with that name');
+    elements.countryList.innerHTML = '';
+    elements.countryInfo.innerHTML = '';
+  })
+}
+
+function renderCountriesList(data) {
+  return data.map(item => {
+    return `
+    <li class="country-item">
+      <img src="${item.flags.svg}" alt="" class="country-flag" width="50" height="50">
+     <h2 class="country-name">${item.name.official}</h2>
+  </li>`
+  }).join('');
+}
+
+function renderCountryInfo(data) {
+  return data.map(item => {
+    return `
+    <p><b>Capital :</b>${item.capital}</p>
+    <p><b>Population :</b>${item.population}</p>
+    <p><b>Languages :</b>${Object.values(item.languages)}</p>`
+  }).join('');
+}
